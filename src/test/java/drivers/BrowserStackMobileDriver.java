0dc23aaf0b774ea5840ba35f54ff1b8236d8fde5
Yuri Kulagin <kulagin1987@gmail.com>
2022-08-18 13:48:23 +0300

    refactoribg

--- src/test/java/drivers/BrowserStackMobileDriver.java
+++ src/test/java/drivers/BrowserStackMobileDriver.java
@@ -26,6 +26,7 @@ public class BrowserStackMobileDriver implements WebDriverProvider {
         mutableCapabilities.setCapability("project", "First Java Project");
         mutableCapabilities.setCapability("build", "browserstack-build-1");
         mutableCapabilities.setCapability("name", "first_test");
+        mutableCapabilities.setCapability("browserstack.idleTimeout", "5");
 
         return new RemoteWebDriver(getBrowserstackUrl(), mutableCapabilities);
     }
